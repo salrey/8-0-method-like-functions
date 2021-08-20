d18394491df355db5fc2aa75050d1f814bc51910
Angie Munoz <angiepmunoz@gmail.com>
2021-08-20 09:55:41 -0400

    included a message to not use specific methods.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Appends an element or multiple elements to the end of an array.
+ * Appends an element or multiple elements to the end of an array. Do not use the push method.
  * @param {Array[]} array - an array of with any kind of elements.
  * @returns {number} the new length of the array.
  */
@@ -7,14 +7,14 @@
 function myPushFunction() {}
 
 /**
- * Removes the last element of the array and returns the removed element or undefined if the array is empty.
+ * Removes the last element of the array and returns the removed element or undefined if the array is empty. Do not use the pop method.
  * @param {Array[]} array - an array of with any kind of elements.
  * @returns {number} the new length of the array .
  */
 function myPopFunction() {}
 
 /**
- * Determine whether an array includes a specific value within its entries.
+ * Determine whether an array includes a specific value within its entries. Do not use the includes method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {*} searchElement - an element of any type that you are searching for in the array.
  * @returns {boolean} returns true or false if the searchElement is found in the array.
@@ -22,7 +22,7 @@ function myPopFunction() {}
 function myIncludesFunction() {}
 
 /**
- * Return the first index in an array where a specified value is found.
+ * Return the first index in an array where a specified value is found. Do not use the indexOf method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {*} searchElement - an element of any type that you are searching for in the array.
  * @returns {number} returns the index at with the searchElement is found or -1.
@@ -30,7 +30,7 @@ function myIncludesFunction() {}
 function myIndexOfFunction() {}
 
 /**
- * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+ * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified. Do not use the slice method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {number} [startIdx] - an optional number representing the starting index of the extraction. The startIdx can be negative.
  * @param {number} [endIdx] - an optional number representing the ending index of the extraction (non-inclusive). The endIdx can be negative.
@@ -39,7 +39,7 @@ function myIndexOfFunction() {}
 function mySliceFunction() {}
 
 /**
- * Creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+ * Creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator. Do not use the join method.
  * @param {Array[*]} array - an array of with any kind of elements
  * @param {string} [separator] - an optional string to separate each pair of adjacent elements of the array. If no separator is specific the default separator should be a comma (",")
  * @returns {string} returns a new array containing the extracted elements
@@ -47,7 +47,7 @@ function mySliceFunction() {}
 function myJoinFunction() {}
 
 /**
- * Returns an array with the elements reverse. The first array element becomes the last, and the last array element becomes the first.
+ * Returns an array with the elements reverse. The first array element becomes the last, and the last array element becomes the first. Do not use the reverse method.
  * @param {Array[*]} array - an array of with any kind of elements
  * @returns {Array[*]} returns a new array with the elements reversed.
  * 
@@ -56,7 +56,7 @@ function myJoinFunction() {}
 function myReverseFunction() {}
 
 /**
- * Returns an array with one or more elements added to the front of the array followed by the original elements.
+ * Returns an array with one or more elements added to the front of the array followed by the original elements. Do not use the unshift method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {*} newElement - an element of any type to add to the front of the array.
  * @returns {Array[*]} returns a new array with the newElement added to the front of the array followed by the original elements.
